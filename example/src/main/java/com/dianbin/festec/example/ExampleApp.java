b1e95deb84548c515c9a6a34d03f3726c8a002da
songwenming <swmyjx@sina.com>
2018-07-01 15:09:31 +0800

    10.33

--- example/src/main/java/com/dianbin/festec/example/ExampleApp.java
+++ example/src/main/java/com/dianbin/festec/example/ExampleApp.java
@@ -3,12 +3,18 @@ package com.dianbin.festec.example;
 import android.app.Application;
 
 import com.dianbin.latte.app.Latte;
+import com.dianbin.latte.ec.icon.FontEcModule;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 public  class ExampleApp extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
         Latte.init(this)
-                .withApiHost("http://127.0.0.1");
+                .withIcons(new FontAwesomeModule())
+                .withIcons(new FontEcModule())
+                .withApiHost("http://127.0.0.1")
+                .Configure();
+
     }
 }
--- example/src/main/java/com/dianbin/festec/example/MainActivity.java
+++ example/src/main/java/com/dianbin/festec/example/MainActivity.java
@@ -2,6 +2,9 @@ package com.dianbin.festec.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.dianbin.latte.app.Latte;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(Latte.getApplication(),"Context已经传入",Toast.LENGTH_LONG).show();
     }
 }
