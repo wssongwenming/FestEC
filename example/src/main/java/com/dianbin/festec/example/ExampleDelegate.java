9c16ef0a029a100e7a70216e27559428387d2fb1
songwenming <swmyjx@sina.com>
2018-07-03 09:10:34 +0800

    10.33

--- example/src/main/java/com/dianbin/festec/example/ExampleDelegate.java
+++ example/src/main/java/com/dianbin/festec/example/ExampleDelegate.java
@@ -3,8 +3,13 @@ package com.dianbin.festec.example;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 import com.dianbin.latte.delegates.LatteDelegate;
+import com.dianbin.latte.net.RestClient;
+import com.dianbin.latte.net.callback.IError;
+import com.dianbin.latte.net.callback.IFailure;
+import com.dianbin.latte.net.callback.ISuccess;
 
 public class ExampleDelegate extends LatteDelegate {
     @Override
@@ -14,6 +19,30 @@ public class ExampleDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        testRestClient();
 
+    }
+    private void testRestClient(){
+        RestClient.builder()
+                .url("http://news.baidu.com/")
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        Toast.makeText(getContext(),response,Toast.LENGTH_LONG).show();
+                    }
+                })
+                .failure(new IFailure() {
+                    @Override
+                    public void onFailure() {
+
+                    }
+                })
+                .error(new IError() {
+                    @Override
+                    public void onError(int code, String msg) {
+                    }
+                })
+                .build()
+                .get();
     }
 }
