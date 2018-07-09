df99c111c89afb11c7c04137c4816f5ed14b2b8d
songwenming <swmyjx@sina.com>
2018-07-09 14:40:14 +0800

    大致完成Restclient

--- example/src/main/java/com/dianbin/festec/example/ExampleApp.java
+++ example/src/main/java/com/dianbin/festec/example/ExampleApp.java
@@ -13,7 +13,7 @@ public  class ExampleApp extends Application{
         Latte.init(this)
                 .withIcons(new FontAwesomeModule())
                 .withIcons(new FontEcModule())
-                .withApiHost("http://127.0.0.1")
+                .withApiHost("http://127.0.0.1/")
                 .Configure();
 
     }
